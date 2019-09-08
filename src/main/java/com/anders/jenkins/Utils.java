8961a6333367997930248a25b03e2ecd6c573ab2
huapuyu <huapuyu@gmail.com>
2019-09-08 14:38:52 +0800

    commit 1

---
+++ src/main/java/com/anders/jenkins/Utils.java
@@ -0,0 +1,13 @@
+package com.anders.jenkins;
+
+public class Utils {
+	
+	public static String getHostName() {
+		return "anders";
+	}
+	
+	public static String getAddress() {
+		return "192.168.1.1";
+	}
+
+}
